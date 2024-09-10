3799f107e02183a7b000ca98f06a479ece49647a
UDHAYAKIRANG <2k21cse170@kiot.ac.in>
2024-09-10 07:24:25 +0530

    my second commit

--- food/src/app/app.component.ts
+++ food/src/app/app.component.ts
@@ -1,10 +1,13 @@
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
+import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
